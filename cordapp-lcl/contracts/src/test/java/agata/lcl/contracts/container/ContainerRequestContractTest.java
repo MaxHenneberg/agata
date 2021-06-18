8edd74779f460d4fc0431daf283de8d783190122
Adrian Mitter <adrian.mitter@tum.de>
2021-06-18 12:11:49 +0200

    Extend unit tests for ContainerRequestContract

--- cordapp-lcl/contracts/src/test/java/agata/lcl/contracts/container/ContainerRequestContractTest.java
+++ cordapp-lcl/contracts/src/test/java/agata/lcl/contracts/container/ContainerRequestContractTest.java
@@ -1,6 +1,5 @@
 package agata.lcl.contracts.container;
 
-import agata.bol.dataholder.Address;
 import agata.bol.dataholder.ContainerInformation;
 import agata.bol.enums.ContainerType;
 import agata.lcl.states.container.ContainerRequestProposal;
@@ -56,5 +55,57 @@ public class ContainerRequestContractTest {
             return null;
         });
     }
+
+    @Test
+    public void shouldFailForNonMatchingContainerType() {
+        ledger(ledgerServices, l -> {
+            l.transaction(tx -> {
+                Party lclCompany = alice.getParty();
+                Party shippingLine = bob.getParty();
+
+                tx.command(Arrays.asList(lclCompany.getOwningKey(), shippingLine.getOwningKey()), new ContainerRequestContract.Commands.AssignContainer());
+
+                ContainerRequestState initiallyProposedState = new ContainerRequestState(shippingLine, lclCompany, lclCompany, "Port A", "Port B", "Any id", ContainerType.Large, null, null);
+                ContainerRequestProposal requestProposal = new ContainerRequestProposal(initiallyProposedState.getLclCompany(), initiallyProposedState.getShippingLine(), initiallyProposedState);
+                tx.input(ContainerRequestContract.ID, requestProposal);
+
+                ContainerRequestState modifiedState = new ContainerRequestState(initiallyProposedState);
+                modifiedState.setVesselName("My vessel");
+                // Set different container type than expected
+                modifiedState.setContainer(new ContainerInformation("123", "456", ContainerType.Small));
+                ContainerRequestProposal assignmentProposal = new ContainerRequestProposal(shippingLine, lclCompany, modifiedState);
+                tx.output(ContainerRequestContract.ID, assignmentProposal);
+
+                return tx.failsWith("type of the assigned container must match the requested type");
+            });
+            return null;
+        });
+    }
+
+    @Test
+    public void shouldFailForInvalidAssignContainerCommand() {
+        ledger(ledgerServices, l -> {
+            l.transaction(tx -> {
+                Party lclCompany = alice.getParty();
+                Party shippingLine = bob.getParty();
+
+                ContainerRequestState initiallyProposedState = new ContainerRequestState(shippingLine, lclCompany, lclCompany, "Port A", "Port B", "Any id", ContainerType.Large, null, null);
+                ContainerRequestProposal requestProposal = new ContainerRequestProposal(initiallyProposedState.getLclCompany(), initiallyProposedState.getShippingLine(), initiallyProposedState);
+                tx.input(ContainerRequestContract.ID, requestProposal);
+
+                ContainerRequestState modifiedState = new ContainerRequestState(initiallyProposedState);
+                modifiedState.setVesselName("My vessel");
+                modifiedState.setContainer(new ContainerInformation("123", "456", ContainerType.Large));
+                // Use LCL company as proposer (which is invalid)
+                ContainerRequestProposal proposal = new ContainerRequestProposal(lclCompany, lclCompany, modifiedState);
+                tx.output(ContainerRequestContract.ID, proposal);
+
+                tx.command(Arrays.asList(lclCompany.getOwningKey(), shippingLine.getOwningKey()), new ContainerRequestContract.Commands.AssignContainer());
+                return tx.failsWith("Proposer must be the shipping line");
+            });
+            return null;
+        });
+    }
+
 
 }
