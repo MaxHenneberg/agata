444d097f9d223034cc9dcf130613a844e1cd5fd2
Adrian Mitter <adrian.mitter@tum.de>
2021-06-25 20:44:52 +0200

    Add API controller for ship loading

---
+++ cordapp-lcl/clients/src/main/java/agata/lcl/bodies/ShipmentDetails.java
@@ -0,0 +1,31 @@
+package agata.lcl.bodies;
+
+import agata.bol.dataholder.FreightCharges;
+import agata.bol.dataholder.Price;
+import agata.bol.enums.Payable;
+import agata.bol.enums.TypeOfMovement;
+import lombok.*;
+import net.corda.core.identity.Party;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class ShipmentDetails {
+    private String containerStateId;
+    private Party shippingLine;
+    private List<String> houseBolIds;
+    private String modeOfInitialCarriage;
+    private String placeOfInitialReceipt;
+    private String bookingNo;
+    private String billOfLadingNo;
+    private List<String> exportReference;
+    private Payable freightPayableAt;
+    private TypeOfMovement typeOfMovement;
+    private List<FreightCharges> freightChargesList;
+    private Price prepaid;
+    private Price collect;
+}
---
+++ cordapp-lcl/clients/src/main/java/agata/lcl/controllers/LoadingController.java
@@ -0,0 +1,65 @@
+package agata.lcl.controllers;
+
+import agata.bol.states.BillOfLadingState;
+import agata.lcl.bodies.ShipmentDetails;
+import agata.lcl.flows.shiploading.ShiploadingAcceptFlow;
+import agata.lcl.flows.shiploading.ShiploadingProposalFlow;
+import agata.lcl.states.shiploading.ShiploadingProposal;
+import net.corda.core.contracts.UniqueIdentifier;
+import net.corda.core.messaging.CordaRPCOps;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/loadings")
+public class LoadingController extends BaseController {
+
+    @Autowired
+    public LoadingController(CordaRPCOps proxy) {
+        super(proxy);
+    }
+
+    @GetMapping()
+    public List<BillOfLadingState> getFinalizedStates() {
+        return this.getStates(BillOfLadingState.class);
+    }
+
+    @GetMapping("/proposals")
+    public List<ShiploadingProposal> getLoadingProposals() {
+        return this.getStates(ShiploadingProposal.class);
+    }
+
+    @PostMapping("/proposals")
+    public ShiploadingProposal addLoadingProposal(@RequestBody ShipmentDetails details) {
+        List<UniqueIdentifier> houseBolIds = details.getHouseBolIds().stream().map(this::toUniqueIdentifier).collect(Collectors.toList());
+        UniqueIdentifier proposalId = this.startFlow(
+                ShiploadingProposalFlow.Initiator.class,
+                this.toUniqueIdentifier(details.getContainerStateId()),
+                details.getShippingLine(),
+                houseBolIds,
+                details.getModeOfInitialCarriage(),
+                details.getPlaceOfInitialReceipt(),
+                details.getBookingNo(),
+                details.getBillOfLadingNo(),
+                details.getExportReference(),
+                details.getFreightPayableAt(),
+                details.getTypeOfMovement(),
+                details.getFreightChargesList(),
+                details.getPrepaid(),
+                details.getCollect()
+        );
+        return this.queryStateById(ShiploadingProposal.class, proposalId);
+    }
+
+    @PostMapping("/proposals/{proposalId}/acceptance")
+    public BillOfLadingState acceptLoadingProposal(@PathVariable String proposalId) {
+        UniqueIdentifier id = this.toUniqueIdentifier(proposalId);
+        this.startFlow(ShiploadingAcceptFlow.Initiator.class, id);
+        return this.queryStateById(ShiploadingProposal.class, id).getProposedState();
+    }
+
+
+}
