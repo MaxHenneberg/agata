4980dc58bb83071b7e463e09c8835b42d4671c6f
Adrian Mitter <adrian.mitter@tum.de>
2021-07-14 09:47:34 +0200

    Fix DummyProposalContract

--- cordapp-lcl/contracts/src/main/java/agata/lcl/contracts/test/DummyProposalContract.java
+++ cordapp-lcl/contracts/src/main/java/agata/lcl/contracts/test/DummyProposalContract.java
@@ -1,7 +1,25 @@
 package agata.lcl.contracts.test;
 
 import agata.lcl.contracts.GenericProposalContract;
+import net.corda.core.contracts.Command;
+import net.corda.core.transactions.LedgerTransaction;
+import org.jetbrains.annotations.NotNull;
 
 public class DummyProposalContract extends GenericProposalContract {
     public static String ID = "agata.lcl.contracts.test.DummyProposalContract";
+
+    @Override
+    protected void extendedVerifyPropose(@NotNull LedgerTransaction tx, @NotNull Command command) {
+
+    }
+
+    @Override
+    protected void extendedVerifyModify(@NotNull LedgerTransaction tx, @NotNull Command command) {
+
+    }
+
+    @Override
+    protected void extendedVerifyAccept(@NotNull LedgerTransaction tx, @NotNull Command command) {
+
+    }
 }
