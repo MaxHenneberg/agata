3142f384beab6a9d9df594dcb556d83f8f252dfe
Adrian Mitter <adrian.mitter@tum.de>
2021-07-16 17:42:08 +0200

    Fix frontend

--- demo-frontend/src/app/accept-container.service.ts
+++ demo-frontend/src/app/accept-container.service.ts
@@ -21,7 +21,7 @@ export class AcceptContainerService {
   }
 
   resolveDeliveryProposalId(proposalId: string): Observable<DeliveryProposalTO> {
-    return this.httpClient.get('/proposals/' + proposalId);
+    return this.httpClient.get('/deliveries/proposals/' + proposalId);
   }
 
   acceptContainer(containerId: string) {
